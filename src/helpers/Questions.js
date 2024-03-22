449c67ddb2b7e57f4609089d2ae72995d8a28ff1
Srinithi2Aravindan <aravindansrinithi@gmail.com>
2024-03-22 18:43:27 +0530

    React-Quiz-app

---
+++ src/helpers/Questions.js
@@ -0,0 +1,36 @@
+
+export const Questions = [
+    {
+    prompt: "What is the largest planet in our solar system?",
+    optionA: "Venus",
+    optionB: "Jupiter",
+    optionC: "Saturn",
+    optionD: "Earth",
+    asnwer: "optionB",
+    },
+    {
+      prompt: "What is the capital city of France?",
+      optionA: "Berlin",
+      optionB: "London",
+      optionC: "Paris",
+      optionD: "Madrid",
+      asnwer: "optionC",
+    },
+    {
+      prompt: "Which of this is not a javascript framework?",
+      optionA: "React",
+      optionB: "Angular",
+      optionC: "Vue",
+      optionD: "Java",
+      asnwer: "optionD",
+    },
+    {
+      prompt: "What is the main ingredient in bread?",
+      optionA: "Eggs",
+      optionB: "Flour",
+      optionC: "Sugar",
+      optionD: "Milk",
+      asnwer:"optionB",
+    }
+  ];
+  
